baa6a8157517b14801f0a5b571dfc0837d5d3210
Ádrian <adrian.fr.fe@gmail.com>
2021-11-17 17:38:42 -0400

    fix: add a default type for button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,11 @@ import styles from './Button.module.css';
 
 export default function Button(props) {
   return (
-    <button type={props.type} onClick={props.onClick} className={styles.button}>
+    <button
+      type={props.type || 'button'}
+      onClick={props.onClick}
+      className={styles.button}
+    >
       {props.children}
     </button>
   );
